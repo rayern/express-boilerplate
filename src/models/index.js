764dcd7d0056af4cb005e4d971b79c8eb3e74863
Saeesh Tendulkar <saeesh.tendulkar@gmail.com>
2023-10-23 09:28:04 +0530

    database credentials corrected

--- src/models/index.js
+++ src/models/index.js
@@ -4,11 +4,7 @@ import createUserModel from "./User.js"
 import createNoteModel from "./Note.js"
 import createSharedNoteModel from "./SharedNote.js"
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
-})
+const sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=no-verify`, { dialect: 'postgres'})
 
 sequelize.authenticate().then(() => {
     console.log('Database connected')
